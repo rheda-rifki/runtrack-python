98603d1df68b67cdf6d6df5fab904f9e30acab9f
Rheda <rheda.rifki@laplateforme.io>
2023-11-22 10:52:53 +0100

    Fin du job07

---
+++ jour03/job07/main.py
@@ -0,0 +1,18 @@
+def determiner_domaine(langage):
+    if langage == "JavaScript":
+        print("Tu es un développeur web.")
+    elif langage == "Python":
+        print("Tu es un développeur IA.")
+    elif langage == "Java":
+        print("Tu es un développeur logiciel.")
+    elif langage == "ReactJS":
+        print("Tu es un développeur mobile.")
+    else:
+        print("Un jour, je serai le meilleur développeur...")
+
+
+determiner_domaine("JavaScript")
+determiner_domaine("Python")
+determiner_domaine("Java")
+determiner_domaine("ReactJS")
+determiner_domaine("C++") 
