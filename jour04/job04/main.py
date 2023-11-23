826560ef323a4c4ab2f1457a52257066efc8c6f5
Rheda <rheda.rifki@laplateforme.io>
2023-11-23 10:38:12 +0100

    Fin du job04

--- jour04/job04/main.py
+++ jour04/job04/main.py
@@ -1,9 +1,9 @@
 def ajouter_mangue():
     fruits = ["pomme", "cerise", "orange", "Melon"]
-    
-    fruits.insert(2, "Mangue")
-    
+
+    fruits[2] = "Mangue"
+
     print(fruits)
 
-
 ajouter_mangue()
+
