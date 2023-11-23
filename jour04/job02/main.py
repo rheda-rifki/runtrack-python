35156e1b7a9421639a1eb29d785312114d8ebd2d
Rheda <rheda.rifki@laplateforme.io>
2023-11-23 10:07:14 +0100

    Fin du job02

---
+++ jour04/job02/main.py
@@ -0,0 +1,8 @@
+def afficher_deuxieme_element():
+    fruits = ["pomme", "cerise", "orange"]
+    
+    if len(fruits) >= 2:
+        deuxieme_element = fruits[1]
+        print(deuxieme_element)
+
+afficher_deuxieme_element()
