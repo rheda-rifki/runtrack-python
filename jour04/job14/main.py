79bbff3162eabff62a955ef2101a96fc41c6eb8d
Rheda <rheda.rifki@laplateforme.io>
2023-11-23 18:24:27 +0100

    Fin du job14

--- jour04/job14/main.py
+++ jour04/job14/main.py
@@ -1,30 +1,18 @@
-def est_separateur(caractere):
-    separateurs = [' ', ',', '.', ';', ':', '!', '?']
-    return caractere in separateurs
+def my_long_word(number, text):
+    words = text.split()
+    result = ""
 
-def my_long_word(longueur_min, phrase):
-    mot_actuel = ""
-    mots_longueur_min = []
+    for word in words:
+        word_length = 0
+        for char in word:
+            if char.isalpha():  # Vérifie si le caractère est une lettre
+                word_length += 1
+        
+        if word_length > number:
+            result += word + " "
 
-    for caractere in phrase:
-        if not est_separateur(caractere):
-            mot_actuel += caractere
-        elif len(mot_actuel) > longueur_min:
-            mots_longueur_min.append(mot_actuel)
-            mot_actuel = ""
-        else:
-            mot_actuel = ""
+    return result.strip()  # Supprime l'espace à la fin s'il y en a un
 
-    if len(mot_actuel) > longueur_min:
-        mots_longueur_min.append(mot_actuel)
-
-    phrase_sortie = ' '.join(mots_longueur_min)
-
-    return phrase_sortie
-
-longueur_minimale = 3
-phrase_entree = "La peur est le chemin vers le côté obscur, la peur mène à la colère, la colère mène à la haine, la haine mène à la souffrance"
-
-resultat = my_long_word(longueur_minimale, phrase_entree)
-
-print("Output :", resultat)
+# Exemple d'utilisation
+output = my_long_word(3, "La peur est le chemin vers le côté obscur, la peur mène à la colère, la colère mène à la haine, la haine mène à la souffrance")
+print("Output:", output)
